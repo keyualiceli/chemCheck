c0c33450c9ebce4452c61b84874dbad4c63cf91c
Brayden <brayden.royston10@gmail.com>
2020-01-19 00:14:20 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -9,3 +9,65 @@ function changeTop() {
     document.getElementById("headerTxt").innerHTML = searchResult;
 
 }
+
+(function() {
+
+    // Set the configuration for your app
+    // TODO: Replace with your project's config object
+    var config = {
+        apiKey: "AIzaSyDhNVT6xwJpCMf2kMtgPkxiXOdVKT6jV7w",
+        authDomain: "chemboi-cd24f.firebaseio.com",
+        databaseURL: "https://chemboi-cd24f.firebaseio.com/",
+        storageBucket: "chemboi-cd24f.firebaseio.com/.appspot.com"
+    };
+    firebase.initializeApp(config);
+
+    input = searchResult;
+
+    chemicals = [["Oxygen", ["Nitrogen", " Dying"]],  ["Straws", ["Paper Straws", " Fuck the Turtles", " Metal Straws"]]];
+
+    
+
+    // Get a reference to the database service
+    var database = firebase.database();
+
+
+    // Get elements
+    const preObject_name = document.getElementById("chem_name");
+    const preObject_env = document.getElementById("env_eff");
+    const preObject_health = document.getElementById("health_eff");
+
+
+    // Create Reference
+    const name_REF = firebase.database().ref().child(input).child("Name")
+    const envEff_REF = firebase.database().ref().child(input).child("EnvEff")
+    const healthEff_REF = firebase.database().ref().child(input).child("HealthEff")
+
+
+    // Sync Object Changes
+    name_REF.on('value', snap => {
+        preObject_name.innerText = JSON.stringify(snap.val(), null, 3);
+    });
+    envEff_REF.on('value', snap => {
+        preObject_env.innerText = JSON.stringify(snap.val(), null, 3);
+    });
+    healthEff_REF.on('value', snap => {
+        preObject_health.innerText = JSON.stringify(snap.val(), null, 3);
+    });
+
+
+
+
+
+    var i;
+    for (i = 0; i < chemicals.length; i++){
+        if (chemicals[i][0]  == input){
+           // dbRefObject.on('value', snap => {
+                //preObject.innerText = JSON.stringify(snap.val(), null, 3);
+            //});
+            document.getElementById("demo").innerHTML = chemicals[i][1];
+
+        }
+    }
+    
+}());
